d60dda8b1805a7a339062ef2b75d059cb053c6f2
BaoYang012 <yangbao@hotmail.com>
2014-09-02 22:27:28 -0700

    Signed-off-by: BaoYang012 <yangbao@hotmail.com>

---
+++ Homework/CH 01/first.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Basic PHP Page</title>
+</head>
+
+<body>
+
+	<!-- Script 1.2 - first.php -->
+
+	<p>This is standard HTML.</p>
+
+	<?php
+	?>
+
+</body>
+</html>
