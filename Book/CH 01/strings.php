f10d487b4a2761e2b971a2289f6bcc69ac365712
BaoYang012 <yangbao@hotmail.com>
2014-09-03 08:09:37 -0700

    Signed-off-by: BaoYang012 <yangbao@hotmail.com>

---
+++ Book/CH 01/strings.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Strings</title>
+</head>
+
+<body>
+	<?php
+		# Script 1.6 - strings.php
+		
+		// Create the variables:
+		$first_name = 'Patrick';
+		$last_name = 'Rothfuss';
+		$book = 'The Name of the Wind';
+		
+		// Print the values:
+		echo "<p>The book <em>$book</em> was written by $first_name $last_name.</p>";
+		
+	?>
+
+</body>
+</html>
