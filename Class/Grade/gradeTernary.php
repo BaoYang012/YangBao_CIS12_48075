31299d0e691a85ddbfb3662473ed904b444a3106
baoyang012 <yangbao@hotmail.com>
2014-09-08 09:28:46 -0700

    Signed-off-by: baoyang012 <yangbao@hotmail.com>

---
+++ Class/Grade/gradeTernary.php
@@ -0,0 +1,31 @@
+<?php
+	/*
+		Bao Yang
+		September 8, 2014
+		Purpos: Illustrate Branching Constructs
+	*/
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Grade</title>
+</head>
+
+<body>
+	<?php
+		//Initialize the input
+		$score=rand(50,100);
+		
+		//Determine the Grade
+		$grade=($score>=90?'A':
+				($score>=80?'B':
+				($score>=70?'C':
+				($score>=60?'D':'F'))));
+		
+		//Output the Results
+		echo "<h1>A score of $score = $grade</h1>";
+	?>
+</body>
+</html>
