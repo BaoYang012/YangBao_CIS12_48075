29924d5a3a90b28d65053e85dfea95a8e4dea384
baoyang012 <yangbao@hotmail.com>
2014-09-10 09:27:02 -0700

    Signed-off-by: baoyang012 <yangbao@hotmail.com>

---
+++ Class/Grade/gradeSwitchLikeIf.php
@@ -0,0 +1,48 @@
+<?php
+	/*
+		Bao Yang
+		September 8, 2014
+		Purpos: Illustrate Branching Constructs
+	*/
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Grade</title>
+</head>
+
+<body>
+	<?php
+		//Initialize the input and declare variables
+		$score=rand(50,100);
+		$grade='';
+		
+		//Determine the Grade
+		switch($score>=90) {
+			case true:$grade='A';
+			default:
+		}
+		switch($score>=80&&$score<90) {
+			case true:$grade='B';
+			default:
+		}
+		switch($score>=70&&$score<80) {
+			case true:$grade='C';
+			default:
+		}
+		switch($score>=60&&$score<70) {
+			case true:$grade='D';
+			default:$grade='F';
+		}
+		switch($score<60) {
+			case true:$grade='F';
+			default:
+		}
+		
+		//Output the Results
+		echo "<h1>A score of $score = $grade</h1>";
+	?>
+</body>
+</html>
--- Class/Grade/inputgrade.php
+++ Class/Grade/inputgrade.php
@@ -15,16 +15,14 @@
 
 <body>
 	<?php
-		//Initialize the input
-		$score=rand(50,100);
-		$grade='';
+		//Utilize the built in capabilities of PHP to get information passed from a form
+		$score=$_GET['score'];
 		
 		//Determine the Grade
-		if($score>=90)$grade='A';
-		if($score>=80&&$score<90)$grade='B';
-		if($score>=70&&$score<80)$grade='C';
-		if($score>=60&&$score<70)$grade='D';
-		if($score<60)$grade='F';
+		$grade=($score>=90?'A':
+				($score>=80?'B':
+				($score>=70?'C':
+				($score>=60?'D':'F'))));
 		
 		//Output the Results
 		echo "<h1>A score of $score = $grade</h1>";
